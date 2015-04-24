8e3d26310b6e7f094ef02630fa74bf8e070cfc26
Adrien REDON <adrien.redon@etu.univ-lyon1.fr>
2015-04-24 10:07:26 +0200

    PHPdoc

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php 
-class App{
-    function __call($method,$action){
+
+class App {
+
+    /**
+     * Register route with HTTP method (get, post, put, patch, delete)
+     * 
+     * @param  string $method Name of the HTTP
+     * @param         $action URL and Callback
+     * @return App    $this   
+     */
+    function __call($method, $action) {
         $this->{$method.$action[0]} = $action[1];
         return $this;
     }
 
-    function run(){
+    /**
+     * Execute action based on the user request
+     * 
+     * @return Execution of the registred Callback
+     */
+    function run() {
         foreach($this as $request => $action)
             if(preg_match("@$request@i", "$_SERVER[REQUEST_METHOD]$_SERVER[REQUEST_URI]", $p))
                 return $action($this, $p);
